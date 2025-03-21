b0463475afe3ea419f4baf16fd6134968e4e2ca0
GABRIEL SILVA SANDES <g2114604@gmail.com>
2025-03-20 21:22:18 -0300

    Initial commit

---
+++ ex001/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ex001
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Olá, Mundo!");
+        }
+    }
+}
---
+++ ex006/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ex006
+{
+    static void Main()
+    {
+        double num;
+        Console.Write("Digite um número: ");
+        num = double.Parse(Console.ReadLine());
+        double ValorBase = num;
+        double exponent = 3;
+        double triplo = num * 3;
+        double dobro = num * 2;
+        double RaizQuadrada = Math.Sqrt(num);
+        Console.WriteLine($"O triplo de {num} é {triplo}");
+        Console.WriteLine($"O dobro de {num} é {dobro}");
+        Console.WriteLine($"A raiz quadrada de {num} é {RaizQuadrada}");
+    }
+}
