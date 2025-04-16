91e8919c2f3fdc8e66995b2357e7b84363a15572
GABRIEL SILVA SANDES <g2114604@gmail.com>
2025-04-16 00:12:32 -0300

    novos exercícios

---
+++ ex019/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ex019 {
+    static void Main() {
+        string[] alunos = new string[4];
+        for(int i = 0; i < alunos.Length; i++) {
+            Console.Write($"Digite o nome do {i+1}º aluno: ");
+            alunos[i] = Console.ReadLine();
+        }
+
+        Random sorteio = new Random();
+
+        int Sorteado = sorteio.Next(alunos.Length);
+
+        Console.WriteLine($"O aluno sorteado foi {alunos[Sorteado]}");
+    }
+}
---
+++ ex020/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class ex020 {
+    static void Main(){
+        string[] alunos = new string[4];
+
+        for (int i = 0; i < alunos.Length; i++) {
+            Console.Write($"Digite o nome do {i + 1}º aluno: ");
+            alunos[i] = Console.ReadLine();
+        }
+        
+        EmbaralharArray(alunos);
+
+        Console.WriteLine("\nAlunos em ordem aleatória:");
+        foreach (string aluno in alunos) {
+            Console.WriteLine(aluno);
+        }
+    }
+
+    static void EmbaralharArray (string[] array) {
+        Random nums = new Random ();
+        for (int i = array.Length - 1; i > 0; i--) {
+            int j = nums.Next(i + 1);
+            string temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
